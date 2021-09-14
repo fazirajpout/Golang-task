3df7be74b845db2cf06c609646a413667d0e93ec
fazirajpout <rajafaizan939@gmail.com>
2021-09-14 08:47:10 -0700

    updatd

--- task.go
+++ task.go
@@ -8,12 +8,19 @@ import (
  )
 func initializeRouter(){
      r :=mux.NewRouter()
+     //Getting list of student
      r.HandleFunc("/students",GetStudents).Methods("GET")
+     //Getting student on bases of id
      r.HandleFunc("/students/{id}",GetStudent).Methods("GET")
+     //Creating Teacher
      r.HandleFunc("/teachers",CreateTeacher).Methods("POST")
+     //Creating Student
      r.HandleFunc("/students",CreateStudent).Methods("POST")
+     //Teacher log 
      r.HandleFunc("/login",Login).Methods("POST")
+     //Update students on basis of id
      r.HandleFunc("/students/{id}",UpdateStudent).Methods("PUT")
+     //Deleting student on basis of id
      r.HandleFunc("/students/{id}",DeleteStudent).Methods("DELETE")
      
      log.Fatal(http.ListenAndServe(":9000",r))
